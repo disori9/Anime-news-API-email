23d7450cda9dd208ed6b9cba68ab968197ab56b2
Ori <disori777@gmail.com>
2024-11-08 11:14:14 +0800

    Add different parameters to url to get better results on news, add article['description'] is None to for loop to avoid errors when there is no description

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import requests
 import send_email as se
 
+topic = 'anime'
 api_key = 'fb63e499cc284155baae7a1541c6447b'
-url = ('https://newsapi.org/v2/everything?q=anime release&from=2024-11-07&to=2024-11-07&'
-       'sortBy=popularity&apiKey=fb63e499cc284155baae7a1541c6447b')
+url = (f'https://newsapi.org/v2/everything?q={topic}&from=2024-11-01&to=2024-11-07&'
+       'sortBy=relevancy&apiKey=fb63e499cc284155baae7a1541c6447b&language=en')
 
 # Request url from given to a variable
 request = requests.get(url)
@@ -14,7 +15,7 @@ content = request.json()
 # Iterate over the articles in the variable content (dict), then message all top articles to user in one email
 email_message = "Subject: Anime News \n"
 for article in content['articles'][:20]:
-    if article['title'] is None:
+    if article['title'] is None or article['description'] is None:
         continue
     email_message = email_message + article['title'] + "\n" + article['description'] + "\n" + article['url'] + 2*"\n"
 
