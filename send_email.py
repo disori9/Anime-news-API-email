e067dd674806b628fcfc411620024bbc68696f99
Ori <disori777@gmail.com>
2024-11-08 10:27:31 +0800

    Add send_email function

---
+++ send_email.py
@@ -0,0 +1,19 @@
+import smtplib
+import ssl
+import os
+
+def send_email(user_message):
+    host = "smtp.gmail.com"
+    port = 465
+
+    username = "violetori99@gmail.com"
+    receiver = username
+    password = os.getenv("PASSWORD")
+
+    context = ssl.create_default_context()
+
+    message_local = user_message
+
+    with smtplib.SMTP_SSL(host, port, context=context) as server:
+        server.login(username, password)
+        server.sendmail(username, receiver, message_local)
