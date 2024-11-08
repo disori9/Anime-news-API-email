0ed282f189ee37b1887f36374c046745c4f930f3
Ori <disori777@gmail.com>
2024-11-08 10:49:09 +0800

    Modify to send news in one email only, removing spam email news one by one. Also sort news by popularity.

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import requests
 import send_email as se
 
 api_key = 'fb63e499cc284155baae7a1541c6447b'
-url = ('https://newsapi.org/v2/everything?q=anime&from=2024-10-08'
-       '&sortBy=publishedAt&apiKey=fb63e499cc284155baae7a1541c6447b')
+url = ('https://newsapi.org/v2/everything?q=anime release&from=2024-11-07&to=2024-11-07&'
+       'sortBy=popularity&apiKey=fb63e499cc284155baae7a1541c6447b')
 
 # Request url from given to a variable
 request = requests.get(url)
@@ -13,10 +13,10 @@ content = request.json()
 
 # Iterate over the articles in the variable content (dict), as it is a list, then email them to user as Subject: title
 # And description as message.
+email_message = "Subject: Anime News \n"
 for article in content['articles']:
-    email_message = f"""\
-Subject: {article['title']}
+    if article['title'] is None:
+        continue
+    email_message = email_message + article['title'] + "\n" + article['description'] + 2*"\n"
 
-{article['description']}
-"""
-    se.send_email(email_message)
+se.send_email(email_message)
